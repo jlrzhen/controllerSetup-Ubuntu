4b2c7e9567872fd788ee4e654370886ad9d41e58
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2022-11-27 22:20:31 -0500

    add setup for rtl8192eu-linux-driver

---
+++ wifiSetup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Mange/rtl8192eu-linux-driver /tmp
+cd /tmp/rtl8192eu-linux-driver
+sudo dkms add .
+sudo dkms install rtl8192eu/1.0
+echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
+echo -e "8192eu\n\nloop" | sudo tee /etc/modules
+echo "options 8192eu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/8192eu.conf
+sudo update-grub; sudo update-initramfs -u
