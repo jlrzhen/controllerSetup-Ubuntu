48cf820a4c702efe65b4bf4e21ebc222c41f58cc
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2022-11-29 11:27:07 -0500

    update terraform install script

--- controllerSetup-Ubuntu.sh
+++ controllerSetup-Ubuntu.sh
@@ -36,10 +36,18 @@ $controller_python_ver get-pip.py --user && $controller_python_ver -m pip instal
 #pip install docker docker-compose
 
 printf "\ninstalling Terraform...\n"
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
 
 printf "\nenabling Terraform tab completion...\n"
 touch ~/.bashrc
