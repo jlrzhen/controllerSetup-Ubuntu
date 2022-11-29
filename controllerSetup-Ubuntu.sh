9d60b115286b1ae9b065842dba0438ccdd82e5d2
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2022-11-29 11:31:43 -0500

    fix path

--- controllerSetup-Ubuntu.sh
+++ controllerSetup-Ubuntu.sh
@@ -26,7 +26,6 @@ sudo apt install -y git curl
 printf "\ninstalling pip and Ansible...\n"
 echo "export PATH=\"$HOME/.local/bin:$PATH\"" >> $HOME/.bashrc
 source $HOME/.bashrc
-source $HOME/.local/bin
 sudo apt install -y python3-distutils python3-testresources
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 $controller_python_ver get-pip.py --user && $controller_python_ver -m pip install --user ansible
